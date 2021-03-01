90c4b89f91364126147db0d1d4aefc4a86e202d5
VJS <minsung_96@naver.com>
2021-03-01 23:49:51 +0900

    Redirection Ready

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import "./Navigation.css"
 
 function Navigation() {
-    return <div>
-        <Link to="/">Home</Link>
+    return <div className="nav-container">
+        <Link to="/">Home <br></br><br></br></Link>
         <Link to="/about">About</Link>
     </div>
 }
